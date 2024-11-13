4079a97982496398ee21a0a86cbb9b17efabaf64
ganeshPund7787 <ganupund7787@gmail.com>
2024-11-13 15:47:31 +0500

    Add new firebase API key

--- client/src/firebase.ts
+++ client/src/firebase.ts
@@ -1,12 +1,17 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "civilhub-bdfc9.firebaseapp.com",
-  projectId: "civilhub-bdfc9",
-  storageBucket: "civilhub-bdfc9.appspot.com",
-  messagingSenderId: "338991441985",
-  appId: "1:338991441985:web:3f0115829f86a33c0c3ec0",
+  authDomain: "civilhub2-46a99.firebaseapp.com",
+  projectId: "civilhub2-46a99",
+  storageBucket: "civilhub2-46a99.firebasestorage.app",
+  messagingSenderId: "530116381193",
+  appId: "1:530116381193:web:dccc2127f531b3c82e46d8",
 };
 
+// Initialize Fireb ase
 export const app = initializeApp(firebaseConfig);
