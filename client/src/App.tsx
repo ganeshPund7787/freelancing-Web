b556eaa0731725f3bfc58c71f085ec3d98ca3a28
ganeshPund7787 <ganupund7787@gmail.com>
2024-11-13 15:15:53 +0500

    Add lazy loading

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import "./global.css";
 import { ToastContainer } from "react-toastify";
 import {
@@ -9,117 +9,125 @@ import {
   Routes,
 } from "react-router-dom";
 
-import SelectRoleFoeSignUp from "./pages/BothUserPages/SelectRoleFoeSignUp";
 import Layout from "./layout/Layout";
 import { useAppSelectore } from "./App/store";
 import ProtectRoute from "./components/ProtectRoutes/ProtectRoute";
-import Profile from "./pages/civilUser/Profile";
 import { CivilProtectRoute } from "./components/ProtectRoutes/CivilProtectRoute";
 import ClientProtectRoute from "./components/ProtectRoutes/ClientProtectRoute";
-import ClientProfile from "./pages/client User/ClientProfile";
-import CreateJobPost from "./components/ClientUser/CreateJobPost";
-import Messages from "./pages/BothUserPages/Messages";
-import Home from "./pages/BothUserPages/Home";
-import Search from "./pages/BothUserPages/Search";
-import SignIn from "./pages/BothUserPages/SignIn";
-import SignUp from "./pages/civilUser/SignUp";
-import SignUpClient from "./pages/client User/SignUpClient";
-import Media from "./pages/BothUserPages/Media";
-import UserProfile from "./pages/BothUserPages/UserProfile";
+
+// Lazy loaded components
+const SelectRoleFoeSignUp = lazy(
+  () => import("./pages/BothUserPages/SelectRoleFoeSignUp")
+);
+const Profile = lazy(() => import("./pages/civilUser/Profile"));
+const ClientProfile = lazy(() => import("./pages/client User/ClientProfile"));
+const CreateJobPost = lazy(
+  () => import("./components/ClientUser/CreateJobPost")
+);
+const Messages = lazy(() => import("./pages/BothUserPages/Messages"));
+const Home = lazy(() => import("./pages/BothUserPages/Home"));
+const Search = lazy(() => import("./pages/BothUserPages/Search"));
+const SignIn = lazy(() => import("./pages/BothUserPages/SignIn"));
+const SignUp = lazy(() => import("./pages/civilUser/SignUp"));
+const SignUpClient = lazy(() => import("./pages/client User/SignUpClient"));
+const Media = lazy(() => import("./pages/BothUserPages/Media"));
+const UserProfile = lazy(() => import("./pages/BothUserPages/UserProfile"));
 
 const App: React.FC = () => {
   const { CurrentCivilUser } = useAppSelectore((state) => state.user);
   const { Client } = useAppSelectore((state) => state.client);
+
   return (
     <BrowserRouter>
       <ToastContainer />
-      <Routes>
-        <Route
-          element={
-            CurrentCivilUser || Client ? <Navigate to={"/"} /> : <Outlet />
-          }
-        >
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up-user" element={<SignUp />} />
-          <Route path="/sign-up-client" element={<SignUpClient />} />
-          <Route path="/select-role" element={<SelectRoleFoeSignUp />} />
-        </Route>
+      <Suspense fallback={<span>Loading...</span>}>
+        <Routes>
+          <Route
+            element={
+              CurrentCivilUser || Client ? <Navigate to={"/"} /> : <Outlet />
+            }
+          >
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/sign-up-user" element={<SignUp />} />
+            <Route path="/sign-up-client" element={<SignUpClient />} />
+            <Route path="/select-role" element={<SelectRoleFoeSignUp />} />
+          </Route>
 
-        <Route element={<ProtectRoute />}>
-          <Route
-            path="/"
-            element={
-              <Layout showHero={false}>
-                <Home />
-              </Layout>
-            }
-          />
+          <Route element={<ProtectRoute />}>
+            <Route
+              path="/"
+              element={
+                <Layout showHero={false}>
+                  <Home />
+                </Layout>
+              }
+            />
+            <Route
+              path="/messages"
+              element={
+                <Layout showHero={false}>
+                  <Messages />
+                </Layout>
+              }
+            />
+            <Route
+              path="/search"
+              element={
+                <Layout showHero={false}>
+                  <Search />
+                </Layout>
+              }
+            />
+            <Route
+              path="/media"
+              element={
+                <Layout showHero={false}>
+                  <Media />
+                </Layout>
+              }
+            />
+            <Route
+              path="/user/:userId"
+              element={
+                <Layout showHero={false}>
+                  <UserProfile />
+                </Layout>
+              }
+            />
+          </Route>
 
-          <Route
-            path="/messages"
-            element={
-              <Layout showHero={false}>
-                <Messages />
-              </Layout>
-            }
-          />
-          <Route
-            path="/search"
-            element={
-              <Layout showHero={false}>
-                <Search />
-              </Layout>
-            }
-          />
-          <Route
-            path="/media"
-            element={
-              <Layout showHero={false}>
-                <Media />
-              </Layout>
-            }
-          />
-          <Route
-            path="/user/:userId"
-            element={
-              <Layout showHero={false}>
-                <UserProfile />
-              </Layout>
-            }
-          />
-        </Route>
+          <Route element={<CivilProtectRoute />}>
+            <Route
+              path="/user-profile"
+              element={
+                <Layout showHero={false}>
+                  <Profile />
+                </Layout>
+              }
+            />
+          </Route>
 
-        <Route element={<CivilProtectRoute />}>
-          <Route
-            path="/user-profile"
-            element={
-              <Layout showHero={false}>
-                <Profile />
-              </Layout>
-            }
-          />
-        </Route>
-
-        <Route element={<ClientProtectRoute />}>
-          <Route
-            path="/client-profile"
-            element={
-              <Layout showHero={false}>
-                <ClientProfile />
-              </Layout>
-            }
-          />
-          <Route
-            path="/create-job-post"
-            element={
-              <Layout showHero={false}>
-                <CreateJobPost />
-              </Layout>
-            }
-          />
-        </Route>
-        <Route path="*" element={<Navigate to={"/"} />} />
-      </Routes>
+          <Route element={<ClientProtectRoute />}>
+            <Route
+              path="/client-profile"
+              element={
+                <Layout showHero={false}>
+                  <ClientProfile />
+                </Layout>
+              }
+            />
+            <Route
+              path="/create-job-post"
+              element={
+                <Layout showHero={false}>
+                  <CreateJobPost />
+                </Layout>
+              }
+            />
+          </Route>
+          <Route path="*" element={<Navigate to={"/"} />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
