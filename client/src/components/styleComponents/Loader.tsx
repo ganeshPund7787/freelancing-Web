99352b4d4ed7fa54923bf1bf6f733c94f746f090
ganeshPund7787 <ganupund7787@gmail.com>
2024-11-14 20:15:37 +0500

    fixed loader position

--- client/src/components/styleComponents/Loader.tsx
+++ client/src/components/styleComponents/Loader.tsx
@@ -1,6 +1,6 @@
 const Loader = () => {
   return (
-    <div className="flex-col gap-4 w-full flex items-center justify-center">
+    <div className="flex-col gap-4 w-full flex items-center justify-center mt-[15%]">
       <div className="w-20 h-20 border-4 border-transparent text-blue-400 text-4xl animate-spin flex items-center justify-center border-t-blue-400 rounded-full">
         <div className="w-16 h-16 border-4 border-transparent text-red-400 text-2xl animate-spin flex items-center justify-center border-t-red-400 rounded-full"></div>
       </div>
